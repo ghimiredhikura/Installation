e68cfedb1e4b32b57ac37bb6ebed354ab8455fb3
Deepak Ghimire <deep@jbnu.ac.kr>
2017-12-05 17:18:07 +0900

    Update Install OpenCV 3.3.1 in Ubuntu 16.04.sh

--- Install OpenCV 3.3.1 in Ubuntu 16.04.sh
+++ Install OpenCV 3.3.1 in Ubuntu 16.04.sh
@@ -2,6 +2,7 @@ OpenCV-Installation
 Install OpenCV 3.3.1 in Ubuntu 16.04
 
 In this post, I will describe step by step instructions for installing OpenCV 3 (C++) on Ubuntu.
+
 Step 1: Update Package
 
     sudo apt-get update
@@ -27,25 +28,15 @@ Step 2: Install OS Libraries
 Step 3: Download OpenCV
 
     git clone https://github.com/opencv/opencv.git
-
     cd opencv
-
     git checkout 3.3.1
-
     cd ..
-
     git clone https://github.com/opencv/opencv_contrib.git
-
     cd opencv_contrib
-
     git checkout 3.3.1
-
     cd ..
-
     cd opencv
-
     mkdir build
-
     cd build
 
 Step 4: Compile
@@ -53,14 +44,29 @@ Step 4: Compile
     cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=opencv/path -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D OPENCV_EXTRA_MODULES_PATH=/extra/module/path -D BUILD_EXAMPLES=ON ..
 
 Step 5: Compile and Install
-
-find out number of CPU cores in your machine
-
-    nproc substitute 4 by output of nproc
+Find out number of CPU cores in your machine 'nproc' substitute 4 by output of nproc
+    
     make -j4
     sudo make install
-    sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
+
+Step 5: Configuretion   
+Open the opencv.conf file with the following code:
+
+    sudo gedit /etc/ld.so.conf.d/opencv.conf
+
+Add the following line at the end of the file(it may be an empty file, that is ok) and then save it:
+
+    /usr/local/lib
+
+Run the following code to configure the library:
+
     sudo ldconfig
 
+Now you have to open another file:
 
-https://medium.com/@vivek.yadav/deep-learning-setup-for-ubuntu-16-04-tensorflow-1-2-keras-opencv3-python3-cuda8-and-cudnn5-1-324438dd46f0
+    sudo gedit /etc/bash.bashrc
+
+Add these two lines at the end of the file and save it:
+
+    PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
+    export PKG_CONFIG_PATH
